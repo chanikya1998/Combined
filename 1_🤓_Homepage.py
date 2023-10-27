da0ef30f4e9609a080ecafb34dfff72f26c3cc15
Chanikya K <68418067+chanikya1998@users.noreply.github.com>
2023-10-26 20:20:09 -0500

    Update 1_🤓_Homepage.py

--- 1_🤓_Homepage.py
+++ 1_🤓_Homepage.py
@@ -5,6 +5,7 @@ st.set_page_config(
     page_icon="👋",
 )
 
+st.title("Med-Kick Dashboards")
 image_path = "medkicklogo.png"
 st.image(image_path, caption="", use_column_width=True)
 st.sidebar.success("Select a page above.")
