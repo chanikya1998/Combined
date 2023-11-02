59ff63fbcae3ff1eff30e5f3ca5a8fd6e8ac926b
Chanikya K <68418067+chanikya1998@users.noreply.github.com>
2023-11-02 10:47:42 -0500

    auth

--- Homepage.py
+++ Homepage.py
@@ -1,12 +1,55 @@
-import streamlit as st
+import pickle
+from pathlib import Path
 
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+
+
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(
     page_title="Med-Kick Dashboard App",
     page_icon="👋",
 )
 
 
-st.title("Financial Dashboards")
-image_path = "medkicklogo.png"
-st.image(image_path, caption="", use_column_width=True)
-st.sidebar.success("Select a page above.")
+# --- USER AUTHENTICATION ---
+names = ["Peter Parker", "Rebecca Miller"]
+usernames = ["pparker", "rmiller"]
+
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+if authentication_status:
+    # ---- READ EXCEL ----
+    @st.cache
+
+
+    st.title("Financial Dashboards")
+    image_path = "medkicklogo.png"
+    st.image(image_path, caption="", use_column_width=True)
+    st.sidebar.success("Select a page above.")
+
+    # ---- HIDE STREAMLIT STYLE ----
+    hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
