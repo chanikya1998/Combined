9ace3dcd24e313247ec50692afa26c58f35fd158
Chanikya K <68418067+chanikya1998@users.noreply.github.com>
2023-11-02 11:04:12 -0500

    auth

--- Homepage.py
+++ Homepage.py
@@ -1,51 +1,48 @@
 import pickle
 from pathlib import Path
+import streamlit as st
+import streamlit_authenticator as stauth
 
-import pandas as pd  # pip install pandas openpyxl
-import plotly.express as px  # pip install plotly-express
-import streamlit as st  # pip install streamlit
-import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+# User data (names, usernames, and hashed passwords)
+names = ["Peter Parker", "Rebecca Miller"]
+usernames = ["pparker", "rmiller"]
+# In practice, you should securely store hashed passwords.
+# For this example, we'll use plain text passwords for simplicity.
+hashed_passwords = {
+    "pparker": "password1",
+    "rmiller": "password2"
+}
 
-
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+# Initialize the Streamlit app
 st.set_page_config(
     page_title="Med-Kick Dashboard App",
     page_icon="👋",
 )
 
-
-# --- USER AUTHENTICATION ---
-names = ["Peter Parker", "Rebecca Miller"]
-usernames = ["pparker", "rmiller"]
-
-# load hashed passwords
-file_path = Path(__file__).parent / "hashed_pw.pkl"
-with file_path.open("rb") as file:
-    hashed_passwords = pickle.load(file)
-
+# Create an instance of the authenticator
 authenticator = stauth.Authenticate(names, usernames, hashed_passwords, cookie_expiry_days=30)
 
+# Login and authentication status
 name, authentication_status, username = authenticator.login("Login", "main")
 
-if authentication_status == False:
+if authentication_status is False:
     st.error("Username/password is incorrect")
 
-if authentication_status == None:
+if authentication_status is None:
     st.warning("Please enter your username and password")
 
 if authentication_status:
-
     st.title("Financial Dashboards")
     image_path = "medkicklogo.png"
     st.image(image_path, caption="", use_column_width=True)
     st.sidebar.success("Select a page above.")
 
-    # ---- HIDE STREAMLIT STYLE ----
+    # Hide Streamlit style
     hide_st_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                header {visibility: hidden;}
-                </style>
-                """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        </style>
+    """
     st.markdown(hide_st_style, unsafe_allow_html=True)
